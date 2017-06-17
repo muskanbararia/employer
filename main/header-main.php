2060b53c19c2091a6190bdc512be98527cf0824a
v786 <jahangirali786@gmail.com>
2017-06-17 11:33:02 +0530

    separated heead

---
+++ main/header-main.php
@@ -0,0 +1,60 @@
+<header id="masthead" class="site-header" role="banner">
+      <div class="container">
+        <a href="index.html" title="Extended" rel="home" class="site-branding">
+                    <h1 class="site-title">
+                          <img src="https://jobify-demos.astoundify.com/premium/wp-content/uploads/sites/3/2016/04/premium-logo.svg" width="" height="" alt="" />
+            
+            <span>Extended</span>
+          </h1>
+          <h2 class="site-description">Jobify Extended</h2>
+        </a>
+
+        <nav id="site-navigation" class="site-primary-navigation">
+          <a href="#site-navigation" class="js-primary-menu-toggle primary-menu-toggle primary-menu-toggle--close">Close</a>
+
+          <form role="search" method="get" id="searchform" class="searchform searchform--header" action="https://jobify-demos.astoundify.com/extended/">
+  
+  <label class="screen-reader-text" for="s">Search for:</label>
+  <input type="text" value="" name="s" id="s" class="searchform__input searchform--header__input" placeholder="Keywords..." />
+  <button type="submit" id="searchsubmit" class="searchform__submit searchform--header__submit"><span class="screen-reader-text">Search</button>
+</form>
+<div class="nav-menu nav-menu--primary"><ul id="menu-main-menu" class="nav-menu nav-menu--primary"><li id="menu-item-99991397" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991397"><a href="how-it-works/index.html">How It Works</a></li>
+<li id="menu-item-99991393" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991393"><a href="pricing/index.html">Pricing</a></li>
+<li id="menu-item-99991382" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-99991382"><a href="#">Candidates</a>
+<ul class="sub-menu">
+  <li id="menu-item-99991392" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991392"><a href="find-a-job/index.html">Find A Job</a></li>
+  <li id="menu-item-99991212" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991212"><a href="companies/index.html">Companies</a></li>
+  <li id="menu-item-99991384" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991384"><a href="submit-your-resume/index.html">Submit Your Resume</a></li>
+  <li id="menu-item-99991383" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991383"><a href="candidate-dashboard/index.html">Candidate Dashboard</a></li>
+</ul>
+</li>
+<li id="menu-item-99991386" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-99991386"><a href="#">Employers</a>
+<ul class="sub-menu">
+  <li id="menu-item-99991391" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991391"><a href="post-a-job/index.html">Post A Job</a></li>
+  <li id="menu-item-99991387" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991387"><a href="find-a-candidate/index.html">Find A Candidate</a></li>
+  <li id="menu-item-99991388" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991388"><a href="pricing/index.html">Pricing</a></li>
+  <li id="menu-item-99991389" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991389"><a href="job-dashboard/index.html">Job Dashboard</a></li>
+</ul>
+</li>
+<li id="menu-item-99991188" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-has-children menu-item-99991188"><a href="#">Pages</a>
+<ul class="sub-menu">
+  <li id="menu-item-99991221" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991221"><a href="blog/index.html">Blog</a></li>
+  <li id="menu-item-99991211" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-has-children menu-item-99991211"><a href="shop/index.html">Shop</a>
+  <ul class="sub-menu">
+    <li id="menu-item-99991210" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991210"><a href="cart-2/index.html">Cart</a></li>
+    <li id="menu-item-99991209" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991209"><a href="checkout-2/index.html">Checkout</a></li>
+  </ul>
+</li>
+  <li id="menu-item-99991218" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991218"><a href="testimonials/index.html">Testimonials</a></li>
+  <li id="menu-item-99991214" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991214"><a href="frequently-asked-questions/index.html">FAQ</a></li>
+  <li id="menu-item-99991216" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991216"><a href="privacy-policy/index.html">Privacy Policy</a></li>
+  <li id="menu-item-99991217" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-99991217"><a href="terms-and-conditions/index.html">Terms and Conditions</a></li>
+</ul>
+</li>
+<li id="menu-item-99991219" class="popup menu-item menu-item-type-post_type menu-item-object-page menu-item-99991219"><a href="signup/index.html" class="popup-trigger-ajax">Sign Up</a></li>
+<li id="menu-item-99991419" class="login menu-item menu-item-type-post_type menu-item-object-page menu-item-99991419"><a href="my-account/index.html" class="popup-trigger-ajax">Login</a></li>
+</ul></div>       </nav>
+
+        <a href="#site-navigation" class="js-primary-menu-toggle primary-menu-toggle primary-menu-toggle--open"><span class="screen-reader-text">Menu</span></a>
+      </div>
+    </header><!-- #masthead -->
