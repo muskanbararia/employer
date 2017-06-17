b7475df10fedde1a1b80b4e40593e68b3e651f71
v786 <jahangirali786@gmail.com>
2017-06-17 11:28:26 +0530

    separated heeader/footer

---
+++ main/footer.php
@@ -0,0 +1,167 @@
+<div class="footer-cta">
+      <div class="container">
+        <h2>Got a question?</h2>
+<p>We're here to help. Check out our FAQs, send us an email or call us at 1 (800) 555-5555</p>
+<p><a href="#" class="button button--type-action">Contact Us</a></p>
+      </div>
+    </div>
+    
+    <footer id="colophon" class="site-footer" role="contentinfo">
+            <div class="footer-widgets">
+        <div class="container">
+          <div class="row">
+             
+            <div class="col-xs-12 col-md-6 col-lg-3">
+              <aside id="text-1" class="widget widget--footer widget_text">     <div class="textwidget"><embed src="https://17314-presscdn-0-16-pagely.netdna-ssl.com/extended/wp-content/uploads/sites/3/2016/04/premium-logo-inverted.svg" type="image/svg+xml" />
+<br></br>
+
+Job Searching Just Got Easy. Use Jobify to run a hiring site and earn money in the process!</div>
+    </aside>            </div>
+             
+            <div class="col-xs-12 col-md-6 col-lg-3">
+              <aside id="pages-1" class="widget widget--footer widget_pages"><h3 class="widget-title widget-title--footer">Site Map</h3>    <ul>
+      <li class="page_item page-item-703"><a href="blog/index.html">Blog</a></li>
+<li class="page_item page-item-99991379"><a href="candidate-dashboard/index.html">Candidate Dashboard</a></li>
+<li class="page_item page-item-2452"><a href="cart-2/index.html">Cart</a></li>
+<li class="page_item page-item-2453"><a href="checkout-2/index.html">Checkout</a></li>
+<li class="page_item page-item-2064"><a href="companies/index.html">Companies</a></li>
+<li class="page_item page-item-1948"><a href="frequently-asked-questions/index.html">FAQ</a></li>
+<li class="page_item page-item-99991381"><a href="find-a-candidate/index.html">Find A Candidate</a></li>
+<li class="page_item page-item-3940"><a href="find-a-job/index.html">Find A Job</a></li>
+<li class="page_item page-item-25 current_page_item"><a href="index.html">Home</a></li>
+<li class="page_item page-item-99991394"><a href="how-it-works/index.html">How It Works</a></li>
+<li class="page_item page-item-7"><a href="job-dashboard/index.html">Job Dashboard</a></li>
+<li class="page_item page-item-8"><a href="jobs/index.html">Jobs</a></li>
+<li class="page_item page-item-2454"><a href="login/index.html">Login</a></li>
+<li class="page_item page-item-99991207"><a href="my-account/index.html">My Account</a></li>
+<li class="page_item page-item-99991191"><a href="post-a-job/index.html">Post A Job</a></li>
+<li class="page_item page-item-99991374"><a href="pricing/index.html">Pricing</a></li>
+<li class="page_item page-item-1762"><a href="privacy-policy/index.html">Privacy Policy</a></li>
+<li class="page_item page-item-2451"><a href="shop/index.html">Shop</a></li>
+<li class="page_item page-item-1673"><a href="signup/index.html">Sign Up</a></li>
+<li class="page_item page-item-99991378"><a href="submit-your-resume/index.html">Submit Your Resume</a></li>
+<li class="page_item page-item-1760"><a href="terms-and-conditions/index.html">Terms and Conditions</a></li>
+<li class="page_item page-item-1719"><a href="testimonials/index.html">Testimonials</a></li>
+    </ul>
+    </aside>            </div>
+             
+            <div class="col-xs-12 col-md-6 col-lg-3">
+                  <aside id="recent-posts-3" class="widget widget--footer widget_recent_entries">   <h3 class="widget-title widget-title--footer">Recent News Articles</h3>   <ul>
+          <li>
+        <a href="canada-adds-12500-jobs-in-modest-july-rebound/index.html">Canada adds 12,500 jobs in modest July rebound</a>
+              <span class="post-date">April 10, 2016</span>
+            </li>
+          <li>
+        <a href="canada-adds-12500-jobs-in-modest-july-rebound-which-is-a-good-sign/index.html">Outsourcing vs. In-House Digital Marketing: Choosing What is Best for a Small Business</a>
+              <span class="post-date">April 6, 2016</span>
+            </li>
+          <li>
+        <a href="fitness-industry-leading-the-charge-on-hiring-in-the-uk-and-ireland/index.html">Canadian Video Game Production Soars 50% In One Year, And Is Catching Up To Film, TV</a>
+              <span class="post-date">April 1, 2016</span>
+            </li>
+        </ul>
+    </aside>                </div>
+             
+            <div class="col-xs-12 col-md-6 col-lg-3">
+              <aside id="text-2" class="widget widget--footer widget_text"><h3 class="widget-title widget-title--footer">Jobify Offices</h3>      <div class="textwidget">Jobify Inc.
+555 Madison Avenue, Suite F-2
+Manhattan, New York 10282
+<br></br>
+Jobify Inc Canada.
+545 Younge St, Suite 11
+Toronto, Ontario M4K 6F4</div>
+    </aside>            </div>
+                      </div>
+        </div>
+      </div>
+      
+      <div class="copyright">
+        <div class="container">
+          <div class="site-info">
+            &copy; 2017 Extended &mdash; All Rights Reserved          </div><!-- .site-info -->
+
+          <div class="footer-social"><a href="http://facebook.com/astoundify"><span class="screen-reader-text">Facebook</span></a>
+<a href="http://twitter.com/astoundify"><span class="screen-reader-text">Twitter</span></a>
+<a href="https://plus.google.com/"><span class="screen-reader-text">Google Plus</span></a>
+<a href="http://instagram.com/"><span class="screen-reader-text">Instagram</span></a>
+<a href="https://pinterest.com/"><span class="screen-reader-text">Pinterest</span></a>
+<a href="https://vimeo.com/astoundify"><span class="screen-reader-text">Vimeo</span></a>
+<a href="http://www.linkedin.com/"><span class="screen-reader-text">Linkedin</span></a>
+</div>
+          <a href="#top" class="btt "><span class="screen-reader-text">Back to Top</span></a>
+        </div>
+      </div>
+    </footer><!-- #colophon -->
+  </div><!-- #page -->
+
+  <div id="ajax-response"></div>
+
+  <script id="tmpl-infoBubble" type="text/template">
+    <# if ( typeof( data.title ) != 'undefined') { #>
+        <a href="{{{ data.href }}}">{{{ data.title }}}</a>
+    <# } #>
+</script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wc_add_to_cart_params = {"ajax_url":"\/extended\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/extended\/?wc-ajax=%%endpoint%%","i18n_view_cart":"View cart","cart_url":"https:\/\/jobify-demos.astoundify.com\/extended\/cart-2\/","is_cart":"","cart_redirect_after_add":"no"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/add-to-cart.min8571.js?ver=3.0.7'></script>
+<script type='text/javascript' src='js/jquery.blockUI.min44fd.js?ver=2.70'></script>
+<script type='text/javascript' src='js/js.cookie.min6b25.js?ver=2.1.4'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var woocommerce_params = {"ajax_url":"\/extended\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/extended\/?wc-ajax=%%endpoint%%"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/woocommerce.min8571.js?ver=3.0.7'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var wc_cart_fragments_params = {"ajax_url":"\/extended\/wp-admin\/admin-ajax.php","wc_ajax_url":"\/extended\/?wc-ajax=%%endpoint%%","fragment_name":"wc_fragments"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/cart-fragments.min8571.js?ver=3.0.7'></script>
+<script type='text/javascript' src='js/core.mine899.js?ver=1.11.4'></script>
+<script type='text/javascript' src='js/widget.mine899.js?ver=1.11.4'></script>
+<script type='text/javascript' src='js/mouse.mine899.js?ver=1.11.4'></script>
+<script type='text/javascript' src='js/slider.mine899.js?ver=1.11.4'></script>
+<script type='text/javascript' src='js/underscore.min4511.js?ver=1.8.3'></script>
+<script type='text/javascript' src='js/backbone.min9632.js?ver=1.2.3'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var _wpUtilSettings = {"ajax":{"url":"\/extended\/wp-admin\/admin-ajax.php"}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/wp-util.min66f2.js?ver=4.7.5'></script>
+<script type='text/javascript' src='js/wp-backbone.min66f2.js?ver=4.7.5'></script>
+<script type='text/javascript' src='js/jquery.deserialize1576.js?ver=1.2.1'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var job_manager_chosen_multiselect_args = {"search_contains":"1"};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/chosen.jquery.minf488.js?ver=1.1.0'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var job_manager_ajax_filters = {"ajax_url":"\/extended\/jm-ajax\/%%endpoint%%\/","is_rtl":"0","i18n_load_prev_listings":"Load previous listings","lang":null};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/ajax-filters.min9786.js?ver=1.26.0'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var jobifySettings = {"ajaxurl":"https:\/\/jobify-demos.astoundify.com\/extended\/wp-admin\/admin-ajax.php","archiveurl":"https:\/\/jobify-demos.astoundify.com\/extended\/jobs\/","i18n":{"magnific":{"tClose":"Close","tLoading":"Loading...","tError":"The content could not be loaded."}},"pages":{"is_job":false,"is_resume":false,"is_testimonials":false}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/jobify.mine149.js?ver=20140416'></script>
+<script type='text/javascript'>
+/* <![CDATA[ */
+var jobifyMapSettings = {"useClusters":"1","overlayTitle":"%d Found","autoFit":"1","trigger":"mouseover","mapOptions":{"zoom":3,"maxZoom":17,"maxZoomOut":3,"gridSize":60,"scrollwheel":true}};
+/* ]]> */
+</script>
+<script type='text/javascript' src='js/app.mina22f.js?ver=20150213'></script>
+<script type='text/javascript' src='js/salvattore.min66f2.js?ver=4.7.5'></script>
+<script type='text/javascript' src='js/main442b.js?ver=20140525'></script>
+<script type='text/javascript' src='js/wp-embed.min66f2.js?ver=4.7.5'></script>
+<script type="text/javascript">window.NREUM||(NREUM={});NREUM.info={"beacon":"bam.nr-data.net","licenseKey":"e20a3d19de","applicationID":"17202079","transactionName":"NQBaN0JUCEMDBkRZCQxKeQBEXAleTQ9fUg8EHA==","queueTime":0,"applicationTime":257,"atts":"GUdZQQpOG00=","errorBeacon":"bam.nr-data.net","agent":""}</script></body>
+
+</html>
